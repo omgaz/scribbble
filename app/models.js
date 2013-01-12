20a3cdb8553f09495275bee150a7029dc0b835a1
Gary Chisholm <garyjchisholm@gmail.com>
2013-01-12 20:37:51 +0000

    Updated and merged changes
    I'm gonna start trying to get my head around backbone js

--- app/models.js
+++ app/models.js
@@ -1,16 +1,31 @@
 var mongoose = require('mongoose'),
     Schema   = mongoose.Schema;
 
-var getTags = function (tags) {
-  return tags.join(',');
-}
+// --  TAGS -- //
+var TagSchema = new Schema({
+  "name": String
+});
 
-var setTags = function (tags) {
-  return tags.split(',');
-}
+mongoose.model("Tag", TagSchema);
 
+
+// --  COMMENTS -- //
+var CommentSchema = new Schema({
+  "body": String,
+  "author": String,
+  "creationDate": { "type": Date, "default": Date.now }
+});
+
+mongoose.model("Comment", CommentSchema);
+
+
+// --  COPY -- //
 var CopySchema = new Schema({
-
+  "body": String,
+  "author": String,
+  "creationDate": { "type": Date, "default": Date.now },
+  "tags": [ObjectId],
+  "comments": [Comment]
 });
 
 mongoose.model("Copy", CopySchema);
